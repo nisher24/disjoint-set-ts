cd37717058209054452af991b4864b269207a2a0
Yuxing <yxdeng@hotmail.com>
2022-02-07 18:39:51 -0500

    Create class TreeNode

--- disjointset.ts
+++ disjointset.ts
@@ -20,3 +20,50 @@ class DisjointSet {
 	// 		question in README.md
 	// }
 }
+
+class TreeNode {
+	private val: number;
+	private parent: TreeNode;
+	private size: number; // number of nodes in the tree rooted at the node
+	private min: number; // the minimum member of the tree rooted at the node
+
+	constructor(val: number) {
+		this.val = val;
+		this.parent = this;
+		this.size = 1;
+		this.min = val;
+	}
+
+	// Getters and setters
+	public get getVal() : number {
+		return this.val;
+	}
+
+	public set setVal(val: number) {
+		this.val = val;
+	}
+
+	public get getParent() : TreeNode {
+		return this.parent;
+	}
+
+	public set setParent(parent: TreeNode) {
+		this.parent = parent;
+	}
+
+	public get getSize() : number {
+		return this.size;
+	} 
+
+	public set setSize(size: number) {
+		this.size = size;
+	}
+
+	public get getMin() : number {
+		return this.min;
+	}
+
+	public set setMin(min: number) {
+		this.min = min;
+	}
+}
